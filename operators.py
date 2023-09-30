f14e9c71c587d5b4f845a74856dac946fe81fadc
Prathyusha-90 <145919144+Prathyusha-90@users.noreply.github.com>
2023-09-30 01:54:16 -0700

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,118 @@
+#Athematicoperator
+#addition
+r = 3
+d = 6
+print(r+d)
+#subtraction
+f = 7
+q = 4
+print(f-q)
+#multiplication
+t = 19
+g = 21
+print(t*g)
+#division
+y = 12
+o = 7
+print(y/o)
+#module
+b = 5
+n = 9
+print(b%n)
+#floordivision
+s = 10
+i = 12
+print(s//i)
+#Assignmentoperator
+m = 6
+m += 4
+print(m)
+m = 6
+m -= 4
+print(m)
+m = 6
+m *= 4
+print(m)
+m = 6
+m %= 4
+print(m)
+m = 6
+m /= 4
+print(m)
+m = 6
+m //= 4
+print(m)
+#logical operator
+#ando perator
+v = 7
+h = 3
+print(v<h and v<h)
+print(v<h & v<h)
+#or operator
+f = 5
+j = 4
+n = 2
+print(f<j or f>n)
+print(f<j | f>n)
+#not operator
+v = 14
+u = 13
+t = 12
+print( not (v<u) and (v<t))
+#comparision operator
+g = 14
+h = 18
+print(g<=h)
+y =  45
+e = 36
+print(y<e)
+t = 34
+u = 23
+print(t>=u)
+r = 23
+y = 56
+print(r<y)
+w = 78
+q = 54
+print(w>q)
+t = 455
+g = 566
+print(t!=g)
+#bitwise operator
+#bitwise and operator
+a = 222
+j = 333
+print(a&j)
+#bitwise or operator
+s = 899
+i = 458
+print( s | i )
+#xor operator
+a = 10
+b = 20
+print( a ^ b )
+#left shift operator
+print(9>>2)
+#right shift operator
+print(5<<4)
+#check address
+f = 11
+print(id(f))
+#is operator
+b = 23
+v = 45
+print(v is b)
+#is not operator
+s = 67
+j = 43
+print(j is not s)
+#in operator
+b = prathyushaoutlook@gmail.com
+print("outlook" in b)
+#not in operator
+participants= ("siri","manasa","deepu")
+name = "mayuka"
+print(name not in participants)
+
+
+
